41836657e809277609f3bb8cb646f7c964e278c8
Sarada <saradawrites@gmail.com>
2026-08-11 22:58:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const CONFIG = Object.freeze({
   API_URL: "https://script.google.com/macros/s/AKfycbwwFqxIGrYPvBQcWEnsUxqoltgTNjau-hK5X01YokqMC1BD5BeFDTWYIEwILYYesRDQ/exec",
   CHANNEL: "ADG_HR_API_V1",
-  FRONTEND_VERSION: "1.5.7",
+  FRONTEND_VERSION: "1.5.8",
   REQUIRED_BACKEND_VERSION: "1.5.7",
   PAGE_SIZE: 20,
   REQUEST_TIMEOUT_MS: 45000
